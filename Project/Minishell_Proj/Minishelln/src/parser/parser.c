b98732547733cfc5452852d274cb9c6d2f576322
Vladimir9595 <vladisac@hotmail.it>
2025-01-27 16:29:26 +0100

    fix: change project structure

---
+++ Project/Minishell_Proj/Minishelln/src/parser/parser.c
@@ -0,0 +1,30 @@
+#include "../typedef.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char **parse_command(const char *command) {
+  char **args = malloc(MAX_ARGS * sizeof(char *));
+  if (!args) {
+    perror("malloc");
+    exit(EXIT_FAILURE);
+  }
+
+  char *cmd_copy = strdup(command);
+  if (!cmd_copy) {
+    perror("strdup");
+    exit(EXIT_FAILURE);
+  }
+
+  char *token = strtok(cmd_copy, " ");
+  int i = 0;
+  while (token && i < MAX_ARGS - 1) {
+    args[i++] = strdup(token);
+    token = strtok(NULL, " ");
+  }
+  args[i] = NULL;
+
+  free(cmd_copy);
+  return args;
+}
